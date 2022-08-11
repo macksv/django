c111c1dcb79295d41eddbfbd6359381328520446
macksv <mike_villanueva@yahoo.com>
2022-08-12 07:55:55 +1000

    cleaned up images

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
