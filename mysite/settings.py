1bcc8c7a703e1b9de53fc5bd42552df59ac0bc55
macksv <mike_villanueva@yahoo.com>
2022-08-11 20:32:13 +1000

    added autocomplete=off on forms and fixed DEBUG on settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,11 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-piwagh^fn=)o4)$g^bn9gj0%(utohq=@9$6p-_1%)h6)onlqip'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = []
 
