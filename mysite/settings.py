3742035ac1b5b2505b2b19feb70989e2e984d637
macksv <mike_villanueva@yahoo.com>
2022-08-12 07:09:46 +1000

    corrected dir structure for static images

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
